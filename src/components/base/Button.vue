0f6aab84997fa687b9227ca91d67cd3b6382ed2a
mahmud farsi <mhfarsi002022@gmail.com>
2024-04-15 14:28:46 +0330

    add icons in icon.vue and add definrprops in Button.vue

---
+++ src/components/base/Button.vue
@@ -0,0 +1,19 @@
+<template>
+    <component :is="tag">
+
+    </component>
+</template>
+
+
+<script setup>
+    import { defineProps } from 'vue';
+    import { bool, string } from 'vue-types';
+
+
+
+    const props = defineProps({
+        tag:string(),
+        icon:string(),
+        isIconOnly:bool().def(false)
+    })
+</script>
